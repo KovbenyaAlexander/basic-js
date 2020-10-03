d1a869d0428536bd64a607ad802b353b539fc142
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-10-03 17:12:45 +0300

    feat: add solution of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error;
+  } else if (arr.length === 0) {
+    return [];
+  }
+
+  const res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] === `--discard-next`) {
+      if (arr[i + 2] === `--discard-prev` || arr[i + 2] === `--double-prev`) {
+        i += 2;
+      } else {
+        i++;
+      }
+    } else if (arr[i] === `--discard-prev`) {
+      res.pop();
+    } else if (arr[i] === `--double-next`) {
+      if (i === arr.length - 1) {
+        continue
+      }
+      res.push(arr[i + 1]);
+    } else if (arr[i] === `--double-prev`) {
+      if (i === 0) {
+        continue
+      }
+      res.push(arr[i - 1]);
+    } else {
+      res.push(arr[i])
+    }
+  }
+
+  return res
+
+}
